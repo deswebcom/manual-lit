873468ec04f59c6c4de6b6f6a2d1d9c5aa34d3a5
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-03-14 11:48:40 +0100

    propiedades computadas con múltiples campos

--- components/dw-age.js
+++ components/dw-age.js
@@ -4,19 +4,31 @@ export class DwAge extends LitElement {
     static get properties() {
         return {
             birthday: { type: String },
+            format: { type: String },
         };
     }
 
-    render() {
-        return html`<span>${this.calculateAge(this.birthday)}</span>`;
+    constructor() {
+        super();
+        this.format = 'yyyy-mm-dd';
     }
 
-    // https://stackoverflow.com/questions/4060004/calculate-age-given-the-birth-date-in-the-format-yyyymmdd
-    calculateAge(date) {
+    render() {
+        return html`<span>${this.calculateAge(this.birthday, this.format)}</span>`;
+    }
+
+    // Inspirado en  https://stackoverflow.com/questions/2945113/how-to-create-a-new-date-in-javascript-from-a-non-standard-date-format
+    calculateAge(date, format) {
         if(!date) {
             return '-';
         }
-        var birthday = +new Date(date);
+        var parts = date.match(/(\d+)/g), 
+            i = 0, fmt = {};
+        if(!parts || parts.length != 3) {
+            return 'Formato de fecha incorrecto';
+        }
+        format.replace(/(yyyy|dd|mm)/g, function(part) { fmt[part] = i++; });
+        var birthday = +new Date(parts[fmt['yyyy']], parts[fmt['mm']]-1, parts[fmt['dd']]);
         if(isNaN(birthday)) {
             return 'Formato de fecha incorrecto';
         }
