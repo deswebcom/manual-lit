acf5a4979e8d7a770aa017c727a79cac87963ec7
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-03-18 11:10:05 +0100

    componente loader

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import './components/dw-message.js';
 import './components/dw-counter.js';
-import './components/dw-show-hide';
-import './components/dw-age';
-import './components/dw-card';
-import './components/dw-tag-list';
+import './components/dw-show-hide.js';
+import './components/dw-age.js';
+import './components/dw-card.js';
+import './components/dw-tag-list.js';
+import './components/dw-loader.js';
