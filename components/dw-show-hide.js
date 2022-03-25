64cab3d65a6dd137fad99b303dd6a712f8c73d9c
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-03-25 11:00:30 +0100

    componente con varios slots

--- components/dw-show-hide.js
+++ components/dw-show-hide.js
@@ -6,12 +6,31 @@ class DwShowHide extends LitElement {
         :host {
             display: block;
         }
-        div {
-            display: none;
-
+        main {
+            border: 1px solid #ddd;
+            padding: 10px;
         }
-        :host([show]) div {
+        .content {
+            display: none;
+        }
+        .trigger {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+        .trigger div {
+            flex-grow: 1;
+        }
+        svg {
+            width: 52px;
+            height: 52px;
+            transition: transform 0.3s linear;
+        }
+        :host([show]) .content {
             display: block;
+        }
+        :host([show]) svg {
+            transform: rotate(90deg);
         }
     `;
 
@@ -29,8 +48,19 @@ class DwShowHide extends LitElement {
 
     render() {
         return html`
-            <p @click="${this.change}">Mostrar / Ocultar</p>
-            <div>Este contenido se ve algunas veces sí y otras no...</div>
+            <main>
+                <section class="trigger" @click=${this.change}>
+                    <div>
+                        <slot name="trigger"></slot>
+                    </div>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z"/></svg>
+                    </span>
+                </section>
+                <section class="content">
+                    <slot name="content"></slot>
+                </section>
+            </main>
         `;
     }
 
